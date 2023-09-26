c0b55787ba9fa62df38a91e11286bba36dfdf368
Sreeraam23 <sreeraam2304@gmail.com>
2023-09-26 15:34:28 +0530

    Updated three files

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 "use strict"
-document.querySelector('button').addEventListener('click')
+    document.querySelector('.message').textContent = "Enter the Message 🙄";   
+
+    document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('input').value);
+    if(guess>secretNum){
+        document.querySelector('.message').textContent = "The number is greater than the Secret Number";
+        score = highscore-1;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.highscore').textContent = score;
+    }
+    else if(guess<secretNum){
+        document.querySelector('.message').textContent = "The number is lesser than the Secret Number";
+        score = highscore-1;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.highscore').textContent = score;
+    }
+    else if(guess === secretNum){
+        document.querySelector('.message').textContent = "The Number is correct🎉🎊🎊🔥🔥";
+        (highscore<score)?highscore = score:highscore;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.highscore').textContent = score;
+    }
+    else{
+        document.querySelector('.message').textContent = "No Number 🛑";
+        score = highscore-1;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.highscore').textContent = score;
+    }
+});
+
